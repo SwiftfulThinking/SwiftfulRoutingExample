b569796686da147b4dac1c34e91642875aff5343
Swiftful Thinking <ndsarno16@gmail.com>
2022-05-02 00:14:37 -0400

    Initial Commit

---
+++ SwiftfulRoutingExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftfulRoutingExample
+//
+//  Created by Nick Sarno on 5/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulRoutingExample/SwiftfulRoutingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulRoutingExampleApp.swift
+//  SwiftfulRoutingExample
+//
+//  Created by Nick Sarno on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulRoutingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
